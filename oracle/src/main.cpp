fcdd7c6b433f8103d381be2dd35af7f4eb5361ee
Jose Luis Pedrosa <jlpedrosa@gmail.com>
2016-05-13 21:16:56 +0200

    added template projects

---
+++ oracle/src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 13, 2016
+ *      Author: jose
+ */
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Arduino.h>
+
+
+void setup()
+{
+
+}
+
+
+void loop()
+{
+
+}

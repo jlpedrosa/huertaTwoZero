8a5d5672b6aad78b2a418390a32a3e34c7ddc666
Jose Luis Pedrosa <jlpedrosa@gmail.com>
2016-05-15 10:25:45 +0200

    added file to keep password

---
+++ capataz/src/InitialSettings.h
@@ -0,0 +1,16 @@
+/*
+ * InitialSettings.h
+ *
+ *  Created on: May 15, 2016
+ *      Author: jose
+ */
+
+#ifndef SRC_INITIALSETTINGS_H_
+#define SRC_INITIALSETTINGS_H_
+
+
+#define WIFI_BSSID "sample"
+#define WIFI_PASSWSD "anotherSample"
+
+
+#endif /* SRC_INITIALSETTINGS_H_ */
